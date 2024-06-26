aa6a7f19b25ee1a2f2618ffcaff47800b388b4da
PNone <urikita8@gmail.com>
2024-06-26 17:26:25 +0300

    Print results as html file

--- run_tests.py
+++ run_tests.py
@@ -40,6 +40,41 @@ def create_summary_html(results) -> str:
 <!DOCTYPE html>
 <html>
 <head>
+
+</head>
+<body>
+
+
+    '''
+    for result in results:
+        html += f'''
+        <button type="button" class="collapsible" style="color:{'green' if result['passed'] else 'red'}">{result['name']}</button>
+<div class="content">
+  <p>{result['summary']}</p>
+</div>
+'''
+
+    html += '''
+</body>
+</html>'''
+
+    html += '''
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
 <style>
 .collapsible {
   background-color: #eee;
@@ -65,33 +100,50 @@ def create_summary_html(results) -> str:
   overflow: hidden;
   background-color: #f1f1f1;
 }
+.collapsible:after {
+  content: '\\02795'; /* Unicode character for "plus" sign (+) */
+  font-size: 13px;
+  color: white;
+  float: right;
+  margin-left: 5px;
+}
+
+.active:after {
+  content: "\\2796"; /* Unicode character for "minus" sign (-) */
+}
 </style>
-</head>
-<body>
+<style>
+.content {
+  padding: 0 18px;
+  background-color: white;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+}
+</style>
 
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+</script>
     '''
-    for result in results:
-        html += f'''
-        <button type="button" class="collapsible" style="color={result['passed']}">Open Collapsible</button>
-<div class="content">
-  <p>{result['summary']}</p>
-</div>
-'''
-
-    html += '''
-</body>
-</html>'''
     return html
 
 
 def normalize_newlines(txt: str) -> str:
     return txt.replace('\r\n', '\n').replace('\r', '\n')
-
-
-def print_divider() -> None:
-    print(
-        f"\n{Fore.CYAN}------------------------------------------------------------{Fore.RESET}\n")
 
 
 def print_colored_text(color: ansi.AnsiFore | str, text: str, before_text: str,
@@ -101,54 +153,59 @@ def print_colored_text(color: ansi.AnsiFore | str, text: str, before_text: str,
     )
 
 
-def print_tests_summary(failed_count: int) -> None:
-    if failed_count == 0:
-        print_colored_text(Fore.GREEN, "All Tests Passed! ", "\n", "\n")
-    else:
-        text = f"{failed_count} {'Test' if failed_count == 1 else 'Tests'} Failed!"
-        print_colored_text(Fore.RED, text, "", "\n\n")
-
-
-def print_failed_test(test_name: str, expected_output: str, actual_output: str) -> str:
+def summarize_failed_test(test_name: str, expected_output: str, actual_output: str) -> str:
     return f"\n{test_name} - Failed!\nExpected Output:\n{expected_output}\nActual Output:{actual_output}\n"
 
 
-def print_failed_test_due_to_exception(test_name: str, expected_output: str,
-                                       exception: str) -> None:
-    print_colored_text(Fore.RED, f"{test_name} - Failed due to an error in the tester!", "\n", "\n")
-    print_colored_text(Fore.BLUE, "Expected Output:", "", "\n")
-    print(f"{expected_output}\n")
-    print_colored_text(Fore.BLUE, "Error:", "", "\n")
-    print(f"{exception}\n")
+def summarize_failed_test_due_to_exception(test_name: str, expected_output: str,
+                                           exception: str) -> str:
+    return f"{test_name} - Failed due to an error in the tester!\nExpected Output:\n{expected_output}\nError:\n{exception}\n"
 
 
 def print_failed_valgrind(test_name: str, exception: str) -> str:
     return f'\n{test_name} has leaks!\n Failed due to an error raised by valgrind!\nError:\n{exception}\n'
 
 
-def execute_test(executable_path: str, args: str, name: str, expected_output: str) -> bool:
+def execute_test(executable_path: str, args: str, name: str, expected_output: str) -> list[dict[str, str]]:
+    results: list[dict[str, str]] = []
     try:
         with subprocess.Popen(f'{executable_path} {args}', shell=True, cwd=getcwd()) as proc:
             try:
                 proc.communicate(timeout=TIMEOUT)
             except subprocess.TimeoutExpired as e:
                 proc.kill()
-                print_failed_test_due_to_exception(name, expected_output,
-                                                   str(e.stderr) if e.stderr else e.stdout)
-                return False
+                results.append({
+                    'name': name,
+                    'summary': summarize_failed_test_due_to_exception(name, expected_output,
+                                                       str(e.stderr) if e.stderr else e.stdout),
+                    'passed': False
+                })
+                return results
     except subprocess.CalledProcessError as e:
-        print_failed_test_due_to_exception(name, expected_output,
-                                           e.stderr if e.stderr else e.stdout)
-        return False
+        results.append({
+            'name': name,
+            'summary': summarize_failed_test_due_to_exception(name, expected_output,
+                                               e.stderr if e.stderr else e.stdout),
+            'passed': False
+        })
+        return results
     except subprocess.TimeoutExpired as e:
-        print_failed_test_due_to_exception(name, expected_output,
-                                           str(e.stderr) if e.stderr else e.stdout)
-        return False
+        results.append({
+            'name': name,
+            'summary': summarize_failed_test_due_to_exception(name, expected_output,
+                                               str(e.stderr) if e.stderr else e.stdout),
+            'passed': False
+        })
+        return results
     except Exception as e:
-        print_failed_test_due_to_exception(name, expected_output, str(e))
-        return False
+        results.append({
+            'name': name,
+            'summary': summarize_failed_test_due_to_exception(name, expected_output, str(e)),
+            'passed': False
+        })
+        return results
 
-    return True
+    return results
 
 
 def execute_valgrind_test(command: str, name: str, results: list[dict[str, str]]) -> None:
@@ -169,18 +226,21 @@ def execute_valgrind_test(command: str, name: str, results: list[dict[str, str]]
 
     except subprocess.CalledProcessError as e:
         results.append({
+            'name': f'{name} - Valgrind',
             'summary': print_failed_valgrind(name, e.stderr if e.stderr else e.stdout),
             'passed': False
         })
         return
     except subprocess.TimeoutExpired as e:
         results.append({
+            'name': f'{name} - Valgrind',
             'summary': print_failed_valgrind(name, str(e.stderr) if e.stderr else e.stdout),
             'passed': False
         })
         return
     except Exception as e:
         results.append({
+            'name': f'{name} - Valgrind',
             'summary': print_failed_valgrind(name, str(e)),
             'passed': False
         })
@@ -189,17 +249,17 @@ def execute_valgrind_test(command: str, name: str, results: list[dict[str, str]]
 
     if 'no leaks are possible' in actual_output:
         results.append({
+            'name': f'{name} - Valgrind',
             'summary': f"\n{name} - no Leaks!\n",
             'passed': True
         })
         return
     else:
         results.append({
+            'name': f'{name} - Valgrind',
             'summary': print_failed_valgrind(name, actual_output),
             'passed': False
         })
-
-        return
 
 
 def run_test(executable_path: str, test: dict[str, str | dict[str, str]],
@@ -209,6 +269,7 @@ def run_test(executable_path: str, test: dict[str, str | dict[str, str]],
         if key not in test:
             name = test.get("name", "<missing>")
             results.append({
+                'name': name,
                 'summary': f"\nTest \"{name}\": {key} missing from test object\n",
                 'passed': False
             })
@@ -225,7 +286,7 @@ def run_test(executable_path: str, test: dict[str, str | dict[str, str]],
         expected_output = normalize_newlines(file.read())
 
     test_success = execute_test(executable_path, args, name, expected_output)
-    if test_success:
+    if len(test_success) == 0:
         output_path = test[OUTPUT_FILE]
 
         with open(output_path, "r", encoding='utf-8') as file:
@@ -233,14 +294,18 @@ def run_test(executable_path: str, test: dict[str, str | dict[str, str]],
 
         if actual_output == expected_output:
             results.append({
+                'name': name,
                 'summary': f"\n{name} - Passed!\n",
                 'passed': True
             })
         else:
             results.append({
-                'summary': print_failed_test(name, expected_output, actual_output),
+                'name': name,
+                'summary': summarize_failed_test(name, expected_output, actual_output),
                 'passed': False
             })
+    else:
+        results += test_success
 
     valgrind_command = f'valgrind --leak-check=full {executable_path} {args}'
     execute_valgrind_test(valgrind_command, name, results)
@@ -298,7 +363,6 @@ def main():
         )
         return
 
-    failed_count = 0
     workdir = sys.argv[TESTS_FOLDER_INDEX]
     tests = get_all_tests_from_json(workdir)
     templates = get_all_templates_from_json(workdir)

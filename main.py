ef98fdbb608980335050baeefbee74bdc40090b0
PNone <urikita8@gmail.com>
2024-06-26 15:04:23 +0300

    Refactor to be a better fit for matam

--- main.py
+++ main.py
@@ -8,12 +8,25 @@ from colorama import init, Fore, ansi
 init()
 
 # Define constants
-TESTS_JSON_FILE_NAME = "student_tests.json"
-STDOUT = 0
-STDERR = 1
+TESTS_JSON_FILE_NAME = "uri_testim.json"
 WORKDIR_INDEX = 1
-EXEC_PATH_INDEX = 2
-EXPECTED_ARGS_AMOUNT = 3
+EXPECTED_ARGS_AMOUNT = 2
+TEMPLATES = 'templates'
+EXECUTABLE = 'executable'
+
+TEST_NAME = 'name'
+TEMPLATE_NAME = 'template'
+PARAMS = 'params'
+OUTPUT_FILE = 'output_file'
+EXPECTED_OUTPUT_FILE = 'expected_output_file'
+
+TESTS_KEYS = (
+    TEST_NAME,
+    TEMPLATE_NAME,
+    PARAMS,
+    OUTPUT_FILE,
+    EXPECTED_OUTPUT_FILE
+)
 
 TIMEOUT = int(environ.get('LOCAL_GRADESCOPE_TIMEOUT', '1'))  # 1 second
 
@@ -57,48 +70,52 @@ def print_failed_test_due_to_exception(test_name: str, expected_output: str, exc
     print(f"{exception}\n")
 
 
-def run_test(executable_path: str, test: dict[str, str]) -> bool:
+def run_test(executable_path: str, test: dict[str, str|dict[str, str]], templates: dict[str, str]) -> bool:
     print_divider()
-    for key in ("name", "input", "output"):
+    for key in TESTS_KEYS:
         if key not in test:
             name = test.get("name", "<missing>")
             print_colored_text(Fore.RED, f"Test \"{name}\": {key} missing from test object", "\n", "\n")
             return False
 
-    name = test["name"]
-    input_data = test["input"].encode()
-    output_data = normalize_newlines(test["output"])
+    args = templates[test[TEMPLATE_NAME]]
+    for param_name, param_value in test[PARAMS].items():
+        args.replace(f':::{param_name}:::', param_value)
 
-    actual_output = ''
+    name = test[TEST_NAME]
+    expected_output_path = test[EXPECTED_OUTPUT_FILE]
+
+    with open(expected_output_path, "r", encoding='utf-8') as file:
+        expected_output = normalize_newlines(file.read())
+
     try:
-        proc = subprocess.Popen(executable_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        try:
-            result = proc.communicate(input=input_data, timeout=TIMEOUT)[STDOUT]
-        except subprocess.TimeoutExpired:
-            proc.kill()
-            result = proc.communicate()
-            # Try using stderr or fallback to stdout
-            result = result[STDERR] if result[STDERR] else result[STDOUT]
-        try:
-            actual_output = normalize_newlines(result.decode('utf-8'))
-        except UnicodeDecodeError:
-            actual_output = normalize_newlines(result.decode('windows-1252'))
-
+        with subprocess.Popen(args, executable=executable_path) as proc:
+            try:
+                proc.communicate(timeout=TIMEOUT)
+            except subprocess.TimeoutExpired as e:
+                proc.kill()
+                print_failed_test_due_to_exception(name, expected_output, str(e.stderr) if e.stderr else e.stdout)
+                return False
     except subprocess.CalledProcessError as e:
-        print_failed_test_due_to_exception(name, output_data, e.stderr if e.stderr else e.stdout)
+        print_failed_test_due_to_exception(name, expected_output, e.stderr if e.stderr else e.stdout)
         return False
     except subprocess.TimeoutExpired as e:
-        print_failed_test_due_to_exception(name, output_data, str(e.stderr) if e.stderr else e.stdout)
+        print_failed_test_due_to_exception(name, expected_output, str(e.stderr) if e.stderr else e.stdout)
         return False
     except Exception as e:
-        print_failed_test_due_to_exception(name, output_data, str(e))
+        print_failed_test_due_to_exception(name, expected_output, str(e))
         return False
 
-    if actual_output == output_data:
+    output_path = test[OUTPUT_FILE]
+
+    with open(output_path, "r", encoding='utf-8') as file:
+        actual_output = normalize_newlines(file.read())
+
+    if actual_output == expected_output:
         print_colored_text(Fore.GREEN, f"{name} - Passed! ", "\n", "\n")
         return True
     else:
-        print_failed_test(name, output_data, actual_output)
+        print_failed_test(name, expected_output, actual_output)
         return False
 
 
@@ -108,6 +125,34 @@ def get_all_tests_from_json(workdir: str) -> list[dict[str, str]] | None:
         with open(tests_file_path, "r", encoding='utf-8') as file:
             json_data = json.load(file)
             return json_data["tests"]
+    except (IOError, json.JSONDecodeError) as e:
+        print_colored_text(Fore.RED, f"Error reading JSON file: {e}", "", "")
+        return None
+    except Exception as e:
+        print_colored_text(Fore.RED, f"Unexpected error reading JSON file: {e}", "", "")
+        return None
+
+
+def get_all_templates_from_json(workdir: str) -> dict[str, str] | None:
+    tests_file_path = path.join(workdir, TESTS_JSON_FILE_NAME)
+    try:
+        with open(tests_file_path, "r", encoding='utf-8') as file:
+            json_data = json.load(file)
+            return json_data[TEMPLATES]
+    except (IOError, json.JSONDecodeError) as e:
+        print_colored_text(Fore.RED, f"Error reading JSON file: {e}", "", "")
+        return None
+    except Exception as e:
+        print_colored_text(Fore.RED, f"Unexpected error reading JSON file: {e}", "", "")
+        return None
+
+
+def get_exec_from_json(workdir: str) -> str | None:
+    tests_file_path = path.join(workdir, TESTS_JSON_FILE_NAME)
+    try:
+        with open(tests_file_path, "r", encoding='utf-8') as file:
+            json_data = json.load(file)
+            return json_data[EXECUTABLE]
     except (IOError, json.JSONDecodeError) as e:
         print_colored_text(Fore.RED, f"Error reading JSON file: {e}", "", "")
         return None
@@ -127,13 +172,14 @@ def main():
 
     failed_count = 0
     workdir = sys.argv[WORKDIR_INDEX]
-    executable_path = sys.argv[EXEC_PATH_INDEX]
     tests = get_all_tests_from_json(workdir)
+    templates = get_all_templates_from_json(workdir)
+    executable = get_exec_from_json(workdir)
     if tests is None:
         return
 
     for test in tests:
-        if not run_test(executable_path, test):
+        if not run_test(executable, test, templates):
             failed_count += 1
 
     print_tests_summary(failed_count)
